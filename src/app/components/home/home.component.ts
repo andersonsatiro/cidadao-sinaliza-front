ecfbde438f88db416d3b5a52c61a48d1cb6c38c6
anderson.paiva <anderson.paiva@quark.tec.br>
2025-07-12 19:25:14 -0300

    [main] - creating profile data and profile menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ChevronDown, LucideAngularModule, Search } from 'lucide-angular';
+import { ChevronDown, LucideAngularModule, Search, Settings } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CommonModule],
 })
 export class HomeComponent {
   readonly searchIcon = Search;
-  readonly chevronDownIcon = ChevronDown
+  readonly chevronDownIcon = ChevronDown;
+  readonly settingsIcon = Settings;
+
+  openProfileMenu: boolean = false;
 }
